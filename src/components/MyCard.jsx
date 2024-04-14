66f073925275a69384aa990f09f17b796492aa77
zatchet <zach@rippas.org>
2024-04-14 16:48:37 -0400

    build

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Typewriter } from 'react-simple-typewriter';
 
 import headshot from '../res/outline.png';
