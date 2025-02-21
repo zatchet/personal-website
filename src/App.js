ff3e11b624e5cbf76e6b6a8b0aa7ab51f3e46f0c
zatchet <zach@rippas.org>
2025-02-21 16:41:53 -0500

    formatting and resume

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-import BashPrompt from './components/BashPrompt';
-import MyCard from './components/MyCard';
-
+import "./App.css";
+import BashPrompt from "./components/BashPrompt";
+import MyCard from "./components/MyCard";
 
 function App() {
   return (
     <>
-      <BashPrompt/>
-      <MyCard title="Title" body="Body"/>
+      <BashPrompt />
+      <MyCard />
     </>
   );
 }
--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Typewriter } from 'react-simple-typewriter';
+import { Typewriter } from "react-simple-typewriter";
 
-import headshot from '../res/outline.png';
-import github from '../res/github.png';
-import resume from '../res/resume.png';
-import email from '../res/email.png';
-import linkedin from '../res/linkedin.png';
+import headshot from "../res/outline.png";
+import github from "../res/github.png";
+import resume from "../res/resume.png";
+import email from "../res/email.png";
+import linkedin from "../res/linkedin.png";
 
-const words = ['', 'Hello, World! My name is Zach, and I like to code.'];
-const words2 = ['', 'I recently graduated from Northeastern University with a degree in Computer Science.'];
-const words3 = ['', 'Check out some of my stuff below ↓'];
+const words = ["", "Hello, World! My name is Zach, and I like to code."];
+const words2 = [
+  "",
+  "I recently graduated from Northeastern University with a degree in Computer Science.",
+];
+const words3 = ["", "Check out some of my stuff below ↓"];
 
 const DELAY_TIME = 2500;
 const FIRST_LINE_TIME = 2700;
@@ -28,7 +31,7 @@ const MyCard = () => {
       setThirdLineDone(true);
     }, DELAY_TIME + FIRST_LINE_TIME + SECOND_LINE_TIME + THIRD_LINE_TIME);
   }, []);
-  
+
   const [firstLineDone, setFirstLineDone] = useState(false);
   const [secondLineDone, setSecondLineDone] = useState(false);
   const [thirdLineDone, setThirdLineDone] = useState(false);
@@ -36,48 +39,69 @@ const MyCard = () => {
   return (
     <div class="card-container">
       <div class="card">
-        <img src={headshot} alt="Zachary Rippas" class="img"/>
+        <img src={headshot} alt="Zachary Rippas" class="img" />
         <div class="card-writer">
           <Typewriter
             words={words}
             cursor={!firstLineDone}
-            cursorStyle='|'
+            cursorStyle="|"
             typeSpeed={35}
             delaySpeed={DELAY_TIME}
           />
-          <br/>
-          <br/>
+          <br />
+          <br />
           <Typewriter
             words={words2}
             cursor={firstLineDone && !secondLineDone}
-            cursorStyle='|'
+            cursorStyle="|"
             typeSpeed={35}
             delaySpeed={DELAY_TIME + FIRST_LINE_TIME}
           />
-          <br/>
-          <br/>
+          <br />
+          <br />
           <Typewriter
             words={words3}
             cursor={firstLineDone && secondLineDone && !thirdLineDone}
-            cursorStyle='|'
+            cursorStyle="|"
             typeSpeed={35}
             delaySpeed={DELAY_TIME + FIRST_LINE_TIME + SECOND_LINE_TIME}
           />
-         <br/>
+          <br />
         </div>
         <div class="links">
-          <a class="link-container" href="https://github.com/zatchet" target="_blank" rel='noreferrer'>
-            <img class="link" src={github} alt="Github"/>
+          <a
+            class="link-container"
+            href="https://github.com/zatchet"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img class="link" src={github} alt="Github" />
           </a>
-          <a class="link-container" href="https://linkedin.com/in/rippas" target="_blank" rel='noreferrer'>
-            <img class="link" src={linkedin} alt="LinkedIn"/>
+          <a
+            class="link-container"
+            href="https://linkedin.com/in/rippas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img class="link" src={linkedin} alt="LinkedIn" />
           </a>
-          <a class="link-container" href="/Rippas_Zachary.pdf" target="_blank" rel='noreferrer' title='Resume'>
-            <img class="link" src={resume} alt="Resume"/>
+          <a
+            class="link-container"
+            href="/Rippas_Zachary.pdf"
+            target="_blank"
+            rel="noreferrer"
+            title="Resume"
+          >
+            <img class="link" src={resume} alt="Resume" />
           </a>
-          <a class="link-container" href="mailto:zach@rippas.org" target="_blank" rel='noreferrer'>
-            <img class="link" src={email} alt="Email"/>
-          </a>      
+          <a
+            class="link-container"
+            href="mailto:zach@rippas.org"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img class="link" src={email} alt="Email" />
+          </a>
         </div>
       </div>
     </div>
